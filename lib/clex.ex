510d696350375536d18cb10f865489f6aaccba07
arpieb <rob@octobang.com>
2019-02-22 16:09:12 -0500

    Initial checkin of mix new stubs

---
+++ lib/clex.ex
@@ -0,0 +1,18 @@
+defmodule Clex do
+  @moduledoc """
+  Documentation for Clex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/clex_test.exs
@@ -0,0 +1,8 @@
+defmodule ClexTest do
+  use ExUnit.Case
+  doctest Clex
+
+  test "greets the world" do
+    assert Clex.hello() == :world
+  end
+end
